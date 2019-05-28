a795d25dc428cf4bf46a18a0cdf7b32596e3fc69
Bryan Campos <bcampos@bp.fi.cr>
2019-05-28 15:07:41 -0600

    Se agregan cambios

---
+++ src/app/models/contact.ts
@@ -0,0 +1,10 @@
+export class Contact{
+
+    public name:string;
+    public telephone:Number;
+
+    constructor(name:string, tel:Number){
+        this.name = name;
+        this.telephone = tel;
+    }
+}
