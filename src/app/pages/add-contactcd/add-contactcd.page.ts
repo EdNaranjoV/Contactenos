8bc4c6f53f0751e3720328cb8446ee41933838e4
EdNaranjoV <eder.naranjo@gbsys.com>
2019-05-29 10:03:29 -0600

    Add 2 pages

---
+++ src/app/pages/add-contactcd/add-contactcd.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-contactcd',
+  templateUrl: './add-contactcd.page.html',
+  styleUrls: ['./add-contactcd.page.scss'],
+})
+export class AddContactcdPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
